8e9044fb8891e0f629be9d8938d60824b7d30113
Humberto Gomez Lopez <hoijof@gmail.com>
2021-06-23 13:48:40 +0200

    background

--- src/src/components/background/index.js
+++ src/src/components/background/index.js
@@ -4,11 +4,12 @@ import backgroundVideo from '../../../resources/background.mp4';
 export default function Background({ children }) {
   return (
     <div className="Background" >
+      <div className="Gradient GradientTop"></div>
       <video className="Video" autoPlay muted loop id="myVideo">
         <source src={backgroundVideo} type="video/mp4" />
       </video>
-      <div className="Gradient"></div>
       {children}
+      <div className="Gradient GradientBottom"></div>
     </div>
   )
 }
--- src/src/components/logo/index.js
+++ src/src/components/logo/index.js
@@ -4,7 +4,7 @@ import nsp from '../../../resources/nsp.svg';
 export default function Logo() {
   return (
     <div className="Logo">
-      <img className="LogoImage" src={nsp} alt="NSP" />
+      <img className="LogoImage" src={nsp} alt="" />
       <h2 className="LogoText">NAUTIC SUR PACIFIC</h2>
       <h3 className="LogoSubtext">Equipamiento y Servicios técnicos</h3>
     </div>
